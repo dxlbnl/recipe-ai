45cbae127abce3f9357631945d10fdda0f73ef0a
Dexter <dexter@dxlb.nl>
2024-03-22 12:47:21 +0100

    fix

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -1,5 +1,5 @@
 import { db } from '$lib/db';
-import { recipes } from '$lib/schema';
+import { recipes } from '$lib/db/schema';
 import { eq } from 'drizzle-orm';
 
 export const load = async ({ params }) => {
