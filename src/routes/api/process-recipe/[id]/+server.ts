8913f00a34d416404809e8b03dbc55da4fffce2b
Dexter <dexter@dxlb.nl>
2024-03-25 11:18:03 +0100

    Test multipart

--- src/routes/api/process-recipe/[id]/+server.ts
+++ src/routes/api/process-recipe/[id]/+server.ts
@@ -6,8 +6,32 @@ export const config = {
 
 // POST url: -> return uuid
 //
+const sleep = (time = 200) => new Promise((resolve) => setTimeout(resolve, time));
+
+async function* processRecipe(id: string) {
+	await sleep();
+	yield ['id: 0', 'event: test'].join('\n') + '\n\n';
+	await sleep();
+	yield ['id: 1', 'event: test'].join('\n') + '\n\n';
+	await sleep();
+	yield ['id: 2', 'event: test'].join('\n') + '\n\n';
+	await sleep();
+	yield ['id: 3', 'event: test'].join('\n') + '\n\n';
+	await sleep();
+	yield ['id: 4', 'event: test'].join('\n') + '\n\n';
+	await sleep();
+	yield ['id: 5j', 'event: test'].join('\n') + '\n\n';
+}
 
 export const GET: RequestHandler = ({ params }) => {
-	console.log(params.id);
-	return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+	if (!params.id) {
+		return new Response('no id found', { status: 400 });
+	}
+
+	return new Response(ReadableStream.from(processRecipe(params.id)), {
+		headers: {
+			'cache-control': 'no-cache',
+			'content-type': 'text/event-stream'
+		}
+	});
 };
